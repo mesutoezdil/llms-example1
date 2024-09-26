83fb243277f73b5c8add76c48edf3daaa0b5aca5
mesutoezdil <mesudozdil@gmail.com>
2024-09-26 15:51:42 +0200

    editing sth for model

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mesutoezdil/llms-example1
+
+go 1.22.6
+
+require github.com/predictionguard/go-client v0.26.0
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/predictionguard/go-client"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func run() error {
+	host := "https://api.predictionguard.com"
+	apiKey := os.Getenv("PGKEY")
+
+	logger := func(ctx context.Context, msg string, v ...any) {
+		s := fmt.Sprintf("msg: %s", msg)
+		for i := 0; i < len(v); i = i + 2 {
+			s = s + fmt.Sprintf(", %s: %v", v[i], v[i+1])
+		}
+		log.Println(s)
+	}
+
+	cln := client.New(logger, host, apiKey)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	prompt := `<|im_start|>system
+You are a helpful code assistant.<|im_end|>
+<|im_start|>user
+Write a Go program that prints out random numbers.<|im_end|>
+<|im_start|>assistant<|im_end|>
+`
+
+	// `Temperature` ve `Model` düzeltildi
+	temp := float32(0.3)
+
+	input := client.CompletionInput{
+		Model:       "Hermes2ProLlama38B", // Model adını string olarak kullanıyoruz
+		Prompt:      prompt,
+		MaxTokens:   1000,
+		Temperature: &temp, // `Temperature` pointer olarak verilmeli
+	}
+
+	resp, err := cln.Completions(ctx, input)
+	if err != nil {
+		return fmt.Errorf("ERROR: %w", err)
+	}
+
+	fmt.Println(resp.Choices[0].Text)
+
+	return nil
+}
